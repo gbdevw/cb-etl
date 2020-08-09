1f06d982193186683fd9d7c00f918590420de77e
guillaume.braibant@outlook.com <guillaume.braibant@outlook.com>
2020-08-09 09:55:17 +0000

    Refact. cb client + config. + dependencies

--- src/main/java/gbdevw/colibri/coinbaseetl/service/syncer/MatchSyncer.java
+++ src/main/java/gbdevw/colibri/coinbaseetl/service/syncer/MatchSyncer.java
@@ -1,29 +1,25 @@
 package gbdevw.colibri.coinbaseetl.service.syncer;
 
 import java.time.Instant;
-import java.util.Optional;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import gbdevw.colibri.coinbaseetl.coinbase.client.CoinbaseClient;
+import gbdevw.colibri.coinbaseetl.coinbase.client.CoinbaseTrade;
 import gbdevw.colibri.coinbaseetl.coinbase.websocket.utils.CoinbaseWsEventAddresses;
 import gbdevw.colibri.coinbaseetl.configuration.CoinbaseProperties;
 import gbdevw.colibri.domain.marketevent.MarketEvent.Match;
 import gbdevw.colibri.domain.utils.Utils.Currency;
 import gbdevw.colibri.domain.utils.Utils.Side;
+import io.quarkus.runtime.Startup;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import io.quarkus.vertx.ConsumeEvent;
 import io.vertx.core.eventbus.EventBus;
-import io.vertx.mutiny.core.Vertx;
-import io.vertx.mutiny.core.buffer.Buffer;
-import io.vertx.mutiny.ext.web.client.HttpResponse;
-import io.vertx.mutiny.ext.web.client.WebClient;
 
 /**
  * Class which fetch the latest trades at the application startup in order
@@ -31,6 +27,7 @@ import io.vertx.mutiny.ext.web.client.WebClient;
  */
 @RegisterForReflection
 @ApplicationScoped
+@Startup
 public class MatchSyncer {
 
     /**
@@ -41,12 +38,7 @@ public class MatchSyncer {
     /**
      * Client to help using a REST API
      */
-    private WebClient client;
-
-    /**
-     * API Base url
-     */
-    private String api;
+    private CoinbaseClient client;
 
     /**
      * Product supported
@@ -62,14 +54,14 @@ public class MatchSyncer {
      * Constructor
      * 
      * @param props Coinbase properties
-     * @param vertx Used to API client
+     * @param client Coinbase API client
      * @param bus Bus used to produce Match events
      */
-    public MatchSyncer (CoinbaseProperties props, Vertx vertx, EventBus bus) {
-        this.client = WebClient.create(vertx);
+    @Inject
+    public MatchSyncer (CoinbaseProperties props, @RestClient CoinbaseClient client, EventBus bus) {
+        this.client = client;
         this.bus = bus;
-        this.api = props.api;
-        this.product = props.product.toLowerCase();
+        this.product = props.product;
     }
 
     /**
@@ -85,46 +77,28 @@ public class MatchSyncer {
             LOG.info("Fetching latest trades");
             
             // Fetch latest trades using Coinbase Pro API
-            HttpResponse<Buffer> response = this.client
-                .getAbs(this.api + "/products/" + this.product + "/trades?limit=100")
-                .ssl(true).sendAndAwait();
-                
-            if(response.statusCode() == 200) {
+           CoinbaseTrade [] trades = this.client.getLatestTrades(this.product.toLowerCase(), 100);
+               
+           for(CoinbaseTrade trade : trades) {
+                // Convert to match
+                Match match = Match.newBuilder()
+                .setTimestamp(Instant.parse(trade.time).toEpochMilli())
+                .setId(Long.parseLong(trade.trade_id))
+                .setUnit(Currency.valueOf(this.product.split("-")[0]))
+                .setQuote(Currency.valueOf(this.product.split("-")[1]))
+                .setSide(Side.valueOf(trade.side.toUpperCase()))
+                .setPrice(Double.parseDouble(trade.price))
+                .setSize(Double.parseDouble(trade.size))
+                .build();
 
-                // Get the results as a Json Array
-                JsonArray array = JsonParser.parseString(response.bodyAsString()).getAsJsonArray();
-
-                // Parse the array and get matches
-                for(int c = 0; c < array.size(); c++) {
-
-                    // Extract the JSON Object
-                    JsonObject trade = array.get(c).getAsJsonObject();
-                    LOG.trace(trade.toString());
-
-                    // Convert to match
-                    Match match = Match.newBuilder()
-                    .setTimestamp(Instant.parse(Optional.of(trade.get("time")).orElseThrow().getAsString()).toEpochMilli())
-                    .setId(Optional.of(trade.get("trade_id")).orElseThrow().getAsLong())
-                    .setUnit(Currency.valueOf(this.product.toUpperCase().split("-")[0]))
-                    .setQuote(Currency.valueOf(this.product.toUpperCase().split("-")[1]))
-                    .setSide(Side.valueOf(Optional.of(trade.get("side")).orElseThrow().getAsString().toUpperCase()))
-                    .setPrice(Optional.of(trade.get("price")).orElseThrow().getAsDouble())
-                    .setSize(Optional.of(trade.get("size")).orElseThrow().getAsDouble())
-                    .build();
-
-                    // Publish match event
-                    this.bus.publish(CoinbaseWsEventAddresses.websocketMatchEventAddress, match);
-                    LOG.trace("Match " + match.getId() + " fetched and published");
-                }
-            }
-            else {
-                throw new Exception("Error in HTTP Response : Code = " + response.statusCode() + " , Reason : " + response.statusMessage());
-            }
+                // Publish match event
+                this.bus.publish(CoinbaseWsEventAddresses.websocketMatchEventAddress, match);
+                LOG.trace("Match " + match.getId() + " fetched and published");
+           }
         }
         catch(Exception ex) 
         {
             LOG.error("Failed to fetch latest trades", ex);
-            throw new RuntimeException(ex);
         }
     }
 }
---
+++ src/test/java/gbdevw/colibri/coinbaseetl/coinbase/client/CoinbaseClientTest.java
@@ -0,0 +1,169 @@
+package gbdevw.colibri.coinbaseetl.coinbase.client;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class CoinbaseClientTest {
+
+    private final static Logger LOG = LoggerFactory.getLogger(CoinbaseClientTest.class.getSimpleName());
+
+    @Inject
+    @RestClient
+    CoinbaseClient client;
+    
+    @Test
+    public void testGetLatestTrades100 () {
+
+        LOG.info("testGetLatestTrades100 - START");
+
+        String product = "btc-usd";
+        int limit = 100;
+
+        // Get the 100 latest trades
+        CoinbaseTrade [] trades = this.client.getLatestTrades(product, limit);
+        assertEquals(trades.length, limit);
+
+        for (CoinbaseTrade coinbaseTrade : trades) {
+            assertNotNull(coinbaseTrade.time);
+            assertNotNull(coinbaseTrade.price);
+            assertNotNull(coinbaseTrade.size);
+            assertNotNull(coinbaseTrade.side);
+            assertNotNull(coinbaseTrade.trade_id);
+        }
+
+        LOG.info("testGetLatestTrades100 - SUCCESS");
+    }
+
+    @Test
+    public void testGetLatestTrades10 () {
+
+        LOG.info("testGetLatestTrades10 - START");
+
+        String product = "btc-usd";
+        int limit = 10;
+
+        // Get the 10 latest trades
+        CoinbaseTrade [] trades = this.client.getLatestTrades(product, limit);
+        assertEquals(trades.length, limit);
+
+        for (CoinbaseTrade coinbaseTrade : trades) {
+            assertNotNull(coinbaseTrade.time);
+            assertNotNull(coinbaseTrade.price);
+            assertNotNull(coinbaseTrade.size);
+            assertNotNull(coinbaseTrade.side);
+            assertNotNull(coinbaseTrade.trade_id);
+        }
+
+        LOG.info("testGetLatestTrades10 - SUCCESS");
+    }
+
+    /**
+     * Test what happens when a limit over 100 is aksed (Coinbase limit)
+     */
+    @Test
+    public void testGetLatestTrades101 () {
+
+        LOG.info("testGetLatestTrades101 - START");
+
+        String product = "btc-usd";
+        int limit = 101;
+
+        try {
+            // Get the 101 latest trades
+            CoinbaseTrade [] trades = this.client.getLatestTrades(product, limit);
+            assertEquals(trades.length, limit);
+
+            for (CoinbaseTrade coinbaseTrade : trades) {
+                assertNotNull(coinbaseTrade.time);
+                assertNotNull(coinbaseTrade.price);
+                assertNotNull(coinbaseTrade.size);
+                assertNotNull(coinbaseTrade.side);
+                assertNotNull(coinbaseTrade.trade_id);
+            }
+
+            LOG.info("testGetLatestTrades101 - FAIL : An excepiton should have been thrown");
+            fail("An excepiton should have been thrown");
+        }
+        catch(Exception ex) {
+            LOG.info("Error while processing request : " + ex.getMessage());
+            LOG.info("testGetLatestTrades101 - SUCCESS");
+        }
+    }
+
+    /**
+     * Test what happens when a limit of 0 is aksed (Invalid)
+     */
+    @Test
+    public void testGetLatestTrades0 () {
+
+        LOG.info("testGetLatestTrades0 - START");
+
+        String product = "btc-usd";
+        int limit = 0;
+
+        try {
+            // Get the 0 latest trades
+            CoinbaseTrade [] trades = this.client.getLatestTrades(product, limit);
+            assertEquals(trades.length, limit);
+
+            for (CoinbaseTrade coinbaseTrade : trades) {
+                assertNotNull(coinbaseTrade.time);
+                assertNotNull(coinbaseTrade.price);
+                assertNotNull(coinbaseTrade.size);
+                assertNotNull(coinbaseTrade.side);
+                assertNotNull(coinbaseTrade.trade_id);
+            }
+
+            LOG.info("testGetLatestTrades0 - FAIL : An excepiton should have been thrown");
+            fail("An excepiton should have been thrown");
+        }
+        catch(Exception ex) {
+            LOG.info("Error while processing request : " + ex.getMessage());
+            LOG.info("testGetLatestTrades0 - SUCCESS");
+        }
+    }
+
+    /**
+     * Test what happens when a limit of -1 is aksed (Invalid)
+     */
+    @Test
+    public void testGetLatestTradesN1 () {
+
+        LOG.info("testGetLatestTradesN1 - START");
+
+        String product = "btc-usd";
+        int limit = -1;
+
+        try {
+            // Get the -1 latest trades
+            CoinbaseTrade [] trades = this.client.getLatestTrades(product, limit);
+            assertEquals(trades.length, limit);
+
+            for (CoinbaseTrade coinbaseTrade : trades) {
+                assertNotNull(coinbaseTrade.time);
+                assertNotNull(coinbaseTrade.price);
+                assertNotNull(coinbaseTrade.size);
+                assertNotNull(coinbaseTrade.side);
+                assertNotNull(coinbaseTrade.trade_id);
+            }
+
+            LOG.info("testGetLatestTradesN1 - FAIL : An excepiton should have been thrown");
+            fail("An excepiton should have been thrown");
+        }
+        catch(Exception ex) {
+            LOG.info("Error while processing request : " + ex.getMessage());
+            LOG.info("testGetLatestTradesN1 - SUCCESS");
+        }
+    }
+}
